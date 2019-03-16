f8602f2af9886f9b3d46c5ef3a5cbf318b35ef2a
caozw0910 <star_caozw@163.com>
2019-03-16 10:59:11 +0800

    导出前端页面实现

--- linayi.web/src/main/java/com/linayi/controller/correct/CorrectController.java
+++ linayi.web/src/main/java/com/linayi/controller/correct/CorrectController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -189,7 +190,7 @@ public class CorrectController extends BaseController {
         }
     }
 
-    @RequestMapping("/exportShareRecord.do")
+    @RequestMapping(value = "/exportShareRecord.do",method = RequestMethod.GET)
     public void exportExcel() {
         Correct correct = new Correct();
         correct.setPageSize(20000);
