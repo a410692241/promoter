5e113588704b67ab92e6f373d6f79c95c9dc9514
caozw0910 <star_caozw@163.com>
2019-03-29 10:06:58 +0800

    max = 50,

---
+++ linayi.service/src/main/java/com/linayi/dto/PromoterSettleDTO.java
@@ -0,0 +1,33 @@
+package com.linayi.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PromoterSettleDTO {
+    //推广商下单员ID
+    private Integer promoterId;
+    //订单Id也是订单编号
+    private Long ordersId;
+    //推广商下单员名
+    private String name;
+    //推广商下单员电话
+    private String mobile;
+    //推广商下单员QQ
+    private String qq;
+    //推广商下单员email
+    private String email;
+    //推广商下单员等级
+    private String promoterLevel;
+    //订单数
+    private Integer sumOrderNo;
+    // 订单总额
+    private BigDecimal sumOrderAmount;
+    // 被投诉或违规推广次数
+    private Integer violationOfPromotionNo;
+    // 推广商收益
+    private BigDecimal profit;
+    // 收益是否结算(true-已结算，false-未结算)
+    private String profitIsBack;
+}
