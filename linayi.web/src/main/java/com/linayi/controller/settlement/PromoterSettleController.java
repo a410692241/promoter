3442bc0554042b394e0647003a28f8c13ee49778
caozw0910 <star_caozw@163.com>
2019-03-29 18:59:01 +0800

    结算信息问题修复

---
+++ linayi.web/src/main/java/com/linayi/controller/settlement/PromoterSettleController.java
@@ -0,0 +1,38 @@
+package com.linayi.controller.settlement;
+
+import com.linayi.controller.BaseController;
+import com.linayi.dto.PromoterSettleDTO;
+import com.linayi.exception.BusinessException;
+import com.linayi.exception.ErrorType;
+import com.linayi.service.promoter.PromoterSettleService;
+import com.linayi.util.ResponseData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/finace")
+public class PromoterSettleController extends BaseController {
+    @Autowired
+    private PromoterSettleService promoterSettleService;
+
+    @PostMapping("/getAllPromoter.do")
+    @ResponseBody
+    public ResponseData getAllPromoter() {
+        try {
+            // 推广商列表[推广商信息，订单信息，当前推广商的收益]
+            List<PromoterSettleDTO> promoterSettleDTOList = promoterSettleService.getAllPromoterDto();
+            return new ResponseData(promoterSettleDTOList);
+        } catch (BusinessException e) {
+            return new ResponseData(e.getErrorType());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseData(ErrorType.SYSTEM_ERROR);
+        }
+    }
+
+}
