7fa60f5018707758cc3270abe90035049f8a2bc2
项目测试 <zhenghongtao3533@dingtalk.com>
2019-05-04 11:44:08 +0800

    过期定时器抛异常，打印过期总数量

--- linayi.timer/src/main/java/com/linayi/timer/PriceExpireTimer.java
+++ linayi.timer/src/main/java/com/linayi/timer/PriceExpireTimer.java
@@ -17,24 +17,26 @@ public class PriceExpireTimer extends QuartzJobBean implements Job {
     @Autowired
     private CorrectService correctService;
 
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        try {
-            System.out.println("Method:价格过期方法开始！");
-            Correct correct = new Correct();
-            correct.setStatus(CorrectStatus.AFFECTED.toString());
-            correct.setEndTime(new Date());
-            //查询审核通过的数据
-            List<Correct> list = correctService.getCorrect(correct);
-            if(list.size()>0){
-                for(Correct corrects : list){
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("Method:价格过期方法开始！");
+        Correct correct = new Correct();
+        correct.setStatus(CorrectStatus.AFFECTED.toString());
+        correct.setEndTime(new Date());
+        //查询审核通过的数据
+        List<Correct> list = correctService.getCorrect(correct);
+        System.out.println("priceExpire totalNums=" + list.size());
+        if (list.size() > 0) {
+            for (Correct corrects : list) {
+                try {
                     correctService.priceExpire(corrects);
+                } catch (Exception e) {
+                    System.out.println("priceExpire has Exception correct_id=" + corrects.getCorrectId());
+                    e.printStackTrace();
                 }
             }
-            System.out.println("Method:价格过期方法结束！！！");
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        System.out.println("Method:价格过期方法结束！！！");
     }
-	
+
 }
