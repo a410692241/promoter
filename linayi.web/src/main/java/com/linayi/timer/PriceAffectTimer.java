85b6858e191c5bda736f72e753b59e6ff8b71bf5
liangxuqiang <l>
2019-05-04 16:39:58 +0800

    去掉定时器任务

--- linayi.web/src/main/java/com/linayi/timer/PriceAffectTimer.java
+++ linayi.web/src/main/java/com/linayi/timer/PriceAffectTimer.java
@@ -15,17 +15,17 @@ public class PriceAffectTimer extends QuartzJobBean {
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("Method:价格生效方法开始！");
-        Correct correct = new Correct();
-        correct.setStatus(CorrectStatus.AUDIT_SUCCESS.toString());
-        //查询审核通过的数据
-        List<Correct> list = correctService.getCorrect(correct);
-        if(list.size()>0){
-            for(Correct corrects : list){
-                correctService.priceAffect(corrects);
-            }
-        }
-        System.out.println("Method:价格生效结束！！！");
+//        System.out.println("Method:价格生效方法开始！");
+//        Correct correct = new Correct();
+//        correct.setStatus(CorrectStatus.AUDIT_SUCCESS.toString());
+//        //查询审核通过的数据
+//        List<Correct> list = correctService.getCorrect(correct);
+//        if(list.size()>0){
+//            for(Correct corrects : list){
+//                correctService.priceAffect(corrects);
+//            }
+//        }
+//        System.out.println("Method:价格生效结束！！！");
     }
 
 
