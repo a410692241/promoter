aab26f758437c415eddb6e8307f38fea86bed831
huzhenqiu <215472164@qq.com>
2019-05-15 15:10:10 +0800

    自定义下单修改

--- linayi.app/src/main/java/com/linayi/controller/order/SelfOrderMessageController.java
+++ linayi.app/src/main/java/com/linayi/controller/order/SelfOrderMessageController.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -102,6 +103,7 @@ public class SelfOrderMessageController extends BaseController{
     //点击有无价格
     @RequestMapping("/handleCustomOrder.do")
     @ResponseBody
+    @Transactional
     public Object Price(@RequestBody Map<String, Object> param) {
         try {
             String priceNum = (String) param.get("priceNum");
