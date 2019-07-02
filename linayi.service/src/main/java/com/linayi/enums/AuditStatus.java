8b06b165cda6a0793f096410ca5f0b6f685e24e5
huzhenqiu <215472164@qq.com>
2019-07-02 11:15:37 +0800

    审核任务后台BUG修复

--- linayi.service/src/main/java/com/linayi/enums/AuditStatus.java
+++ linayi.service/src/main/java/com/linayi/enums/AuditStatus.java
@@ -3,7 +3,8 @@ package com.linayi.enums;
 public enum AuditStatus {
     NOT_AUDIT("未审核"),
     AUDIT_SUCCESS("审核通过"),
-    AUDIT_FAIL("价格错误");
+    AUDIT_FAIL_EXPIRED("价格错误过期"),
+    AUDIT_FAIL_CORRECT("价格错误纠错");
 
 
     public String priceTypeName;
